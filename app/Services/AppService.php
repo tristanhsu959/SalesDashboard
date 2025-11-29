aefd1686852daff87cc220b27b42ae674f75c0fb
Tristan <tristan0309@gmail.com>
2025-11-29 14:36:56 +0800

    modify app service & authorization trait

--- app/Services/AppService.php
+++ app/Services/AppService.php
@@ -15,39 +15,21 @@ class AppService
 		// $this->_repository = $partyRepository;
 	}
 	
+	/* 取All Menu List (權限設定用)
+	 * @params: 
+	 * @return: array
+	 */
+	public function getMenu()
+	{
+		return $this->getMenuFromConfig();
+	}
+	
 	/* 取有授權的Menu (登入驗後)
 	 * @params: 
 	 * @return: array
 	 */
 	public function getAuthorizeMenu()
 	{
-		#1.取登入User Permission
-		$signinUser = $this->getSigninUserInfo();
-		$userPermission = $signinUser['Permission'];
-		
-		#2.取功能選單-ALL
-		$menuConfig = $this->getMenu();
-		
-		#3.驗證有權限的選單, 只要驗證到功能即可
-		$authMenu = [];
-		
-		foreach($menuConfig as $key => $group)
-		{
-			if ($this->hasGroupPermission($group['groupCode'], $userPermission))
-			{
-				$authMenu[$key] = $group;
-				$authItems = [];
-				
-				foreach($group['items'] as $action)
-				{
-					if ($this->hasActionPermission($group['groupCode'], $action['actionCode'], $userPermission))
-						$authItems[] = $action;
-				}
-				
-				$authMenu[$key]['items'] = $authItems;
-			}
-		}
-		
-		return $authMenu;
+		return $this->getMenuByPermission();
 	}
 }
--- app/Traits/AuthorizationTrait.php
+++ app/Traits/AuthorizationTrait.php
@@ -32,10 +32,44 @@ trait AuthorizationTrait
 	 * @params: 
 	 * @return: array
 	 */
-	public function getMenu()
+	public function getMenuFromConfig()
 	{
 		return config('web.menu');
 	}
 	
-	
+	/* 取有授權的Menu (登入驗後)
+	 * @params: 
+	 * @return: array
+	 */
+	public function getMenuByPermission()
+	{
+		#1.取登入User Permission
+		$signinUser = $this->getSigninUserInfo();
+		$userPermission = $signinUser['Permission'];
+		
+		#2.取功能選單-ALL
+		$menuConfig = $this->getMenu();
+		
+		#3.驗證有權限的選單, 只要驗證到功能即可
+		$authMenu = [];
+		
+		foreach($menuConfig as $key => $group)
+		{
+			if ($this->hasGroupPermission($group['groupCode'], $userPermission))
+			{
+				$authMenu[$key] = $group;
+				$authItems = [];
+				
+				foreach($group['items'] as $action)
+				{
+					if ($this->hasActionPermission($group['groupCode'], $action['actionCode'], $userPermission))
+						$authItems[] = $action;
+				}
+				
+				$authMenu[$key]['items'] = $authItems;
+			}
+		}
+		
+		return $authMenu;
+	}
 }
