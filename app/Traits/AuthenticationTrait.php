a681fd0f40a8ac7a55a782378fa49645a4c46b43
Tristan <tristan0309@gmail.com>
2025-12-05 21:54:06 +0800

    ad config & auth for testing

--- app/Traits/AuthenticationTrait.php
+++ app/Traits/AuthenticationTrait.php
@@ -11,12 +11,62 @@ use Log;
 /* 認證 */
 trait AuthenticationTrait
 {
+	#Refactor
 	/* AD登入驗證(由trait決定驗證方式)
 	 * @params: string
 	 * @params: string
 	 * @return: array
 	 */
 	public function authenticationAD($account, $password)
+	{
+		#C:\openldap\sysconf\ldap.conf for local dev
+		#無法匿名連線(除LDAP外)
+		
+		$result = [];
+		
+		$connectionConfig = config('web.auth.ad.connection');
+		#必須是Distinquished Name:cn= , base dn
+		$connectionConfig['username'] = "cn={$account},{$connectionConfig['base_dn']}"; 
+		$connectionConfig['password'] = $password;
+		
+		try 
+		{
+			$connection = new Connection($connectionConfig);
+			$connection->connect();
+			
+			/*
+			"CN=林 XX,OU=T16000 資訊處,OU=8way_a00 八方雲集國際股份有限公司,OU=八方雲集國際股份有限公司,DC=8way,DC=com,DC=tw"
+			cn=中文名, title, ou, displayname=英+中, memof, company, department, employeeid, samaccountname, mail, mobile
+			*/
+			$result = $connection->query()->where('samaccountname', '=', $account)->first();
+			
+			#只取需要的資訊
+			$adInfo['company'] 		= $result['company'][0];
+			$adInfo['department'] 	= $result['department'][0];
+			$adInfo['title'] 		= $result['title'][0];
+			$adInfo['displayname'] 	= $result['displayname'][0]; #=>FirstName LastName CNName
+			$adInfo['employeeid'] 	= $result['employeeid'][0]; #=>CNName
+			$adInfo['name'] 		= Str::remove(' ', $result['name'][0]); #=>CNName
+			$adInfo['mail'] 		= $result['mail'][0];
+			
+			return $adInfo;
+		} 
+		catch (\LdapRecord\Auth\BindException $e) 
+		{
+			Log::channel('webSysLog')->error('AD驗證失敗:' . $e->getMessage(), [ __class__, __function__]);
+			Log::channel('webSysLog')->error('ErrorCode:' . $e->getDetailedError()->getErrorCode());
+			Log::channel('webSysLog')->error('DetailError:' . $e->getDetailedError()->getErrorMessage());
+			Log::channel('webSysLog')->error('DiagnosticMessage:' .  $e->getDetailedError()->getDiagnosticMessage());
+			return FALSE;
+		}
+	}
+	
+	/* AD登入驗證(由trait決定驗證方式)
+	 * @params: string
+	 * @params: string
+	 * @return: array
+	 */
+	public function OldauthenticationAD($account, $password)
 	{
 		#fake data
 		/*return [
--- config/web/auth.php
+++ config/web/auth.php
@@ -9,6 +9,22 @@ return [
 			'base_dn'       	=> 'dc=8way,dc=com,dc=tw',
 			'username'			=> '', #testing : '8waytw\LDAP or LDAP' | 'cn=LDAP,dc=local,dc=com'
 			'password'			=> '', #testing : 'a12345678!@'
+			
+			'use_ssl'       	=> FALSE, 	#ssl or tls只能二選一
+			'use_tls'       	=> TRUE,	#389 Port, 文件建議選項 
+			#'use_sasl'      	=> FALSE, 	#測試帳號LDAP要設為FALSE / 其它帳號要設為TRUE(不知為何)
+			'version'       	=> 3,
+			'timeout'       	=> 5,
+			'follow_referrals'	=> FALSE,
+		],
+	],
+	#Old Setting
+	'ad2' => [
+		'connection' => [
+			'hosts'				=> ['DC03.8way.com.tw'],
+			'base_dn'       	=> 'dc=8way,dc=com,dc=tw',
+			'username'			=> '', #testing : '8waytw\LDAP or LDAP' | 'cn=LDAP,dc=local,dc=com'
+			'password'			=> '', #testing : 'a12345678!@'
 			#'port'         	=> 389, #636 ldaps
 			#'protocol'     	=> 'ldap://',
 				
