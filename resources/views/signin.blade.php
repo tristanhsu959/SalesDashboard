1f692225aab6da5abb0c85c2893040da36d031fe
8WAYTW\tristan.hsu <tristan.hsu@8way.com.tw>
2025-12-15 10:10:20 +0800

    ui signin

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -26,16 +26,16 @@
 	
 	<form action="{{ route('signin.post') }}" method="post" id="signinForm">
 		@csrf
-		<div class="input-field field-blue-light">
+		<div class="input-field field-light-blue">
 			<input type="text" class="form-control" id="adAccount" name="adAccount" value="{{ $viewModel->adAccount }}" maxlength="20" placeholder=" " required>
 			<label for="adAccount" class="form-label">Account</label>
 			<span class="domain-text">@8way.com.tw</span>
 		</div>
-		<div class="input-field field-blue-light">
+		<div class="input-field field-light-blue">
 			<input type="password" class="form-control" id="adPassword" name="adPassword" placeholder=" " maxlength="20" required>
 			<label for="adPassword" class="form-label">Password</label>
 		</div>
-		<button id="btnSignin" type="button" class="btn btn-outline-danger">Sign In</button>
+		<button id="btnSignin" type="button" class="btn btn-signin">Sign In</button>
 	</form>
 </div>
 
