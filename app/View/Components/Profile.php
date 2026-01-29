75047571c172fc9fa752fef8ba588dc52963b2d6
8WAYTW\tristan.hsu <tristan.hsu@8way.com.tw>
2026-01-29 16:13:26 +0800

    rename file

---
+++ app/View/Components/Profile.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Traits\AuthTrait;
+use App\Models\CurrentUser;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+
+class Profile extends Component
+{
+	use AuthTrait;
+	
+	public $currentUser;
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        $this->currentUser = $this->getCurrentUser(); 
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+		return view('components.profile');
+    }
+	
+}
