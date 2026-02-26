a5e392fccff5e70da87624797142bdfc23c80f0a
8WAYTW\tristan.hsu <tristan.hsu@8way.com.tw>
2026-02-26 17:35:24 +0800

    menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,22 +1,47 @@
 <!-- Menu component -->
-
-<nav x-data='{menus:@json($menus), currentPath:@json($currentPath)}' class="menu drawer left active">
-	<div class="logo">
-		<div class="logo-wrapper">
-			<img src="{{ asset('images/logo.png') }}" />
-		</div>
+{{--
+<nav x-data='{menus:@json($menus), currentPath:@json($currentPath)}' class="m l max left menu">
+	<header>
+		<img src="{{ asset('images/logo.png') }}" />
+	</header>
+	
+	<template x-for="(groups, key) in menus" :key="key">
+	<div class="responsive">
+		<span x-text="key"></span>
+		<ul class="list">
+			<template x-for="(item, index) in groups" :key="index">
+			<li>
+				<a :href="item.url" :class="currentPath.includes(item.url) ? 'active' : '' " class="responsive round">
+					<i x-text="item.style.icon"></i>
+					<span x-text="item.name"></span>
+				</a>
+			</li>
+			</template>
+		</ul>
 	</div>
+  	</template>
+</nav>
+--}}
+
+<nav x-data='{menus:@json($menus), currentPath:@json($currentPath)}'  class="menu drawer1 left active">
+	<header>
+		<img src="{{ asset('images/logo.png') }}" />
+	</header>
 	
 	<template x-for="(groups, key) in menus">
-		<details :open="groups.some(item => currentPath.startsWith(item.url))">
+		<details x-data="{ isOpen: groups.some(item => currentPath.startsWith(item.url)) }" :open="isOpen" @toggle="isOpen = $el.open">
 			<summary>
-				<i>folder</i>
 				<span x-text="key"></span>
-				<i class="none">arrow_drop_down</i>
+				<i class="none" x-text="isOpen ? 'stat_minus_1':'chevron_forward'"></i>
 			</summary>
 			
 			<template x-for="item in groups">
-				<a :href="item.url" x-text="item.name" :class="currentPath.includes(item.url) ? 'active' : '' " class="responsive"></a>
+			<div class="item">
+				<a :href="item.url" :class="[currentPath.includes(item.url) ? 'active' : '', item.style.color]" class="responsive">
+					<i x-text="item.style.icon"></i>
+					<span x-text="item.name" ></span>
+				</a>
+			</div>
 			</template>
 		</details>
 	</template>
