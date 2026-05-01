a61083c8e704633a1cea571ec3caf4adcabb7312
8WAYTW\tristan.hsu <tristan.hsu@8way.com.tw>
2026-05-01 21:58:33 +0800

    profile ui

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -1,4 +1,3 @@
-@use(App\Enums\Area)
 
 <dialog x-data='{profile:@json($profile), areaOptions:@json($areaOptions), signoutRoute:@json($signoutRoute)}' class="left small" id="profile">
 	<header>
@@ -11,14 +10,17 @@
 	<div class="dialog-body">
 		<div class="info-head">
 			<i class="fill">person_pin</i>
-			<p x-text="profile.employeeId"></p>
-			<p x-text="profile.displayName" class="name"></p>
-			<p x-text="profile.mail"></p>
+			<!--p x-text="profile.employeeId"></p-->
+			<p x-text="profile.userDisplayName" class="name"></p>
+			<p x-text="profile.email"></p>
+			<button class="transparent circle orange white-text profile-edit">
+				<i>person_edit</i>
+			</button >
 		</div>
 		<div class="info-body">
 			<p>
 				<span x-text="profile.department"></span>
-				<span x-text="profile.title"></span>
+				<!--span x-text="profile.title"></span-->
 			</p>
 			<p>
 				<span>管理區域</span>
@@ -26,10 +28,12 @@
 			</p>
 			<p x-show="profile.roleArea.length > 0" class="row wrap auth-area">
 				<template x-for="area in profile.roleArea">
-					<a class="chip border orange white-text" x-text="areaOptions[area]"></a>
+					<button class="chip round medium small-elevate secondary white-text">
+						<span x-text="areaOptions[area]">Input</span>
+					</button >
 				</template>
 			</p>
-			<p x-text="profile.company"></p>
+			<!--p x-text="profile.company"></p-->
 		</div>
 	</div>
 	<a :href="signoutRoute" class="btn-logout button extend circle">
