33643b6317d8771c642f743e8366c92c25306eb0
8WAYTW\tristan.hsu <tristan.hsu@8way.com.tw>
2026-05-04 23:02:29 +0800

    menu icon sample

--- resources/views/components/action-bar.blade.php
+++ resources/views/components/action-bar.blade.php
@@ -5,6 +5,11 @@
 			<i>arrow_back</i>
 		</a>
 		<h6 class="max small" x-html="breadcrumb.join('<i>chevron_right</i>')"></h6>
+		
+		<!--button class="circle transparent menu" @click="$store.menu.active = !$store.menu.active">
+			<i x-show="!$store.menu.active">menu</i>
+			<i x-show="$store.menu.active">menu_open</i>
+		</button-->
 		<a :href="homeRoute" x-show="!isHome" class="button circle transparent">
 			<i>home</i>
 		</a>
